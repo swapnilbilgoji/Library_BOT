aaf286414b6793216e2ab2025ab94acbd2e66253
Swapnil Bilgoji <138004287+swapnilbilgoji@users.noreply.github.com.>
2025-11-23 17:58:38 +0530

    change

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
 #from langchain.prompts import PromptTemplate
-from langchain.chains import RetrievalQA
+#from langchain.chains import RetrievalQA
+from langchain_classic.chains import RetrievalQA
 from langchain.docstore.document import Document
 
 # ---------------------------
